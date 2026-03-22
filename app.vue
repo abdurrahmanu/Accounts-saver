a24fbb38c953b1e98aa7264ef62abe8cae4fe040
unknown <ahmadabdulrahman103@gmail.com>
2026-03-22 08:40:42 +0100

    update

--- app.vue
+++ app.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="pb-24 text-gray-900 font-sans mt-23 selection:bg-none selet-none">
-    <AppHeader />
-    <div class="max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-x-4">
-      <div v-if="addNewAccount">
-        <AddAccountForm />
-        <AddAccountJSON />
-      </div>
-      <div v-else>
-        <SearchAndFilters />
-        <!-- <InstallApp /> -->
+  <div>
+    <NuxtPwaManifest />
+    <div class="pb-24 text-gray-900 font-sans mt-23 selection:bg-none selet-none">
+      <AppHeader />
+      <div class="max-w-5xl mx-auto grid grid-cols-1 lg:grid-cols-3 gap-x-4">
+        <div v-if="addNewAccount">
+          <AddAccountForm />
+          <AddAccountJSON />
+        </div>
+        <div v-else>
+          <SearchAndFilters />
+          <!-- <InstallApp /> -->
           <Collections />
           <AccountList />
         </div>
+      </div>
     </div>
+    <ToolBar />
+    
+    <AppModal :toggle="toggleEditAccountModal">
+      <EditAccount :account="editingAccount"  />
+    </AppModal>
+    
+    <AppModal :toggle="toggleEditCollectionModal">
+      <EditCollection />
+    </AppModal>
   </div>
-  <ToolBar />
-
-  <AppModal :toggle="toggleEditAccountModal">
-    <EditAccount :account="editingAccount"  />
-  </AppModal>
-
-  <AppModal :toggle="toggleEditCollectionModal">
-    <EditCollection />
-  </AppModal>
 </template>
 
 <script setup lang="ts">
